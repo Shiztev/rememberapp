1c9e8ba4bd339ab6dde54b7bc5f975ecb5a6d1dd
Stevie Alvarez <stevie.6strings@gmail.com>
2020-12-07 19:31:56 -0500

    abstract comment skeleton

--- rememory.py
+++ rememory.py
@@ -5,3 +5,12 @@ can make a note in just a few seconds.
 
 @author Stevie Alvarez
 '''
+
+# Create a note: UI with a few text fields
+
+# Store a note: in a file/SQL(?)
+
+# 'Read' stored notes: display the notes to review and remember.
+    # Edit stored notes: alter the content of a note
+
+# Delete stored notes: remove notes from storage
