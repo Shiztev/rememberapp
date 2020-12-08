46ef66ceb1ba2e40ccc621c208eb4161a14ffb2c
Stevie Alvarez <stevie.6strings@gmail.com>
2020-12-07 19:27:16 -0500

    init commit

---
+++ rememory.py
@@ -0,0 +1,7 @@
+'''
+Make quick notes of anything you come accross. Whether it's 
+a doctors appointment or something you heard on the radio, you
+can make a note in just a few seconds.
+
+@author Stevie Alvarez
+'''
